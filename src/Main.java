02c2153157d6fa75ada419dd5fe87e6abca27618
Sayanaann <alexsayana@gmail.com>
2022-11-21 10:49:28 +0530

    subtraction

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main {
         double sum = i + j;
         return sum;
     }
-
+    //SUB
+    public static double sub(double i, double j) {
+        double sub = i - j;
+        return sub;
+    }
     public static void main(String[] args) {
 
         System.out.println("Ready to Calculate");
